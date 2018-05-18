b8893a4be23cb2db8ccd7eae0f80fb4c1bd7f772
Rafael Reis <rafaelreistp@gmail.com>
2018-05-17 21:06:49 -0300

    Iniciado painel com THF carregado

---
+++ painel/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { ThfModule } from '@totvs/thf-ui';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app.routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    ThfModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
